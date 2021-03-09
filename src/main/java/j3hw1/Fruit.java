0d4e252d9f56225f1c00c1a940f75656e196708e
anamaksimova <19cherrytree91@gmail.com>
2021-03-09 20:46:51 +0500

    java3 lesson1

---
+++ src/main/java/j3hw1/Fruit.java
@@ -0,0 +1,20 @@
+package j3hw1;
+
+public class Fruit {
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public Fruit() {
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/j3hw1/Orange.java
@@ -0,0 +1,8 @@
+package j3hw1;
+
+public class Orange extends Fruit{
+    public Orange() {
+        super(1.5f);
+    }
+
+}
